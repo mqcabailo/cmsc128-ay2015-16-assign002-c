eb339b1d42a9b1118c2571d79e1d87a3d4cc78d8
mqcabailo <matthewqcabailo@gmail.com>
2016-03-11 22:51:29 +0800

    created library

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+#include "myLib.h"
 
 int main(){
     int choice;
---
+++ myLib.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+
+int getHammingDistance(char[] str1, char[] str2){
+	
+}
