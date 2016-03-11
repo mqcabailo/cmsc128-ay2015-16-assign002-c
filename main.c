a969bc3da394dac285e05bf2b9061b118ea4e774
mqcabailo <matthewqcabailo@gmail.com>
2016-03-11 23:31:28 +0800

    Added getHammingDistance() to myLib.h

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include <string.h>
 #include "myLib.h"
 
+
 int main(){
     int choice;
-    
+    char str1[50];
+    char str2[50];
     do{
         printf("[1] Get Hamming Distance\n");
         printf("[2] Count Substring Pattern\n");
@@ -15,8 +17,18 @@ int main(){
         printf("[7] Exit\n");
         printf("Enter choice:");
         scanf("%d", &choice);
+        
         switch(choice){
             case 1:
+
+                printf("Enter string 1: ");
+                scanf(" ");    
+                fgets(str1, sizeof(str1), stdin);
+                
+                printf("Enter string 2: ");
+                fgets(str2, sizeof(str2), stdin);
+
+                getHammingDistance(str1,str2);
                 break;
             case 2:
                 break;
--- myLib.h
+++ myLib.h
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int getHammingDistance(char[] str1, char[] str2){
-	
+int getHammingDistance(char str1[], char str2[]){
+	int i;
+	int count = 0;
+	if(strlen(str1) == strlen(str2)){
+		for(i = 0; i<strlen(str1); i+=1){
+			if(str1[i] != str2[i])
+				count+=1;
+		}
+	}
+
+	return count;
 }
+
